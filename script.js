6d750789c0a54b279bed0c2bf17362af302b9b71
Jonathan Walker <jonwalker302@gmail.com>
2021-09-09 21:37:24 -0400

    Add form to submit a new book

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let myLibrary = [new Book('Title1', 'Author1', 100), new Book('Title2', 'Author2', 101)];
-window.onload = displayBooks();
+const bookContainer = document.querySelector('.book-container');
+
+window.onload = displayAllBooks();
+document.querySelector('.add-form').addEventListener('submit', addBookToLibrary);
 
 function Book(title, author, pages) {
   this.title = title;
@@ -8,35 +11,39 @@ function Book(title, author, pages) {
 }
 
 function addBookToLibrary() {
-  let title = '';
+  let title = document.querySelector('input[name=title]').value;
   let author = '';
   let pages = '';
   let book = new Book(title, author, pages);
 
   myLibrary.push(book);
+  displayBook(book);
 }
 
-function displayBooks() {
-  const bookContainer = document.querySelector('.book-container');
+function displayBook(book) {
+  const bookDiv = document.createElement('div');
+  const titleP = document.createElement('p');
+  const authorP = document.createElement('p');
+  const pagesP = document.createElement('p');
+
+  bookDiv.classList.add('book');
+  titleP.classList.add('title');
+  authorP.classList.add('author');
+  pagesP.classList.add('pages');
+
+  titleP.textContent = book.title;
+  authorP.textContent = `By: ${book.author}`;
+  pagesP.textContent = `Length: ${book.pages}`;
+
+  bookDiv.appendChild(titleP);
+  bookDiv.appendChild(authorP);
+  bookDiv.appendChild(pagesP);
+
+  bookContainer.appendChild(bookDiv);
+}
+
+function displayAllBooks() {
   myLibrary.forEach(book => {
-    const bookDiv = document.createElement('div');
-    const titleP = document.createElement('p');
-    const authorP = document.createElement('p');
-    const pagesP = document.createElement('p');
-
-    bookDiv.classList.add('book');
-    titleP.classList.add('title');
-    authorP.classList.add('author');
-    pagesP.classList.add('pages');
-
-    titleP.textContent = book.title;
-    authorP.textContent = `By: ${book.author}`;
-    pagesP.textContent = `Length: ${book.pages}`;
-
-    bookDiv.appendChild(titleP);
-    bookDiv.appendChild(authorP);
-    bookDiv.appendChild(pagesP);
-
-    bookContainer.appendChild(bookDiv);
+    displayBook(book);
   });
 }
